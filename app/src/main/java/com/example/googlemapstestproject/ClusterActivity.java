623d097bf30d1cf4ce882943a64316d0b7f140ab
Sanam Lama <sanamltamang@gmail.com>
2019-07-03 20:19:49 -0400

    Floating Action Button added

--- app/src/main/java/com/example/googlemapstestproject/ClusterActivity.java
+++ app/src/main/java/com/example/googlemapstestproject/ClusterActivity.java
@@ -1,9 +1,13 @@
 package com.example.googlemapstestproject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,16 +17,25 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterManager;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public abstract class ClusterActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 //    private ClusterManager<MyItem> clusterManager;
     private GoogleMap googleMap;
-
+    FloatingActionButton googleFAB;
+    FloatingActionButton launcherFAB;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +44,24 @@ public abstract class ClusterActivity extends AppCompatActivity implements OnMap
 
         initMap();
 
+        FloatingActionButton floating_button = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        googleFAB = (FloatingActionButton) findViewById(R.id.googleFAB);
+        launcherFAB = (FloatingActionButton) findViewById(R.id.launcherFAB);
+
+        floating_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(ClusterActivity.this, "Floating Action Button Clicked", Toast.LENGTH_SHORT).show();
+
+                if (!isFABOpen) {
+                    showFABMenu();
+                }
+                else {
+                    closeFABMenu();
+                }
+            }
+        });
+//        new TestAsync().execute();
     }
 
     // initializing the map
@@ -51,5 +82,90 @@ public abstract class ClusterActivity extends AppCompatActivity implements OnMap
     }
 
     protected abstract void startDemo(GoogleMap googleMap);
+
+    boolean isFABOpen;
+    private void showFABMenu() {
+        isFABOpen = true;
+        googleFAB.animate().translationX(-getResources().getDimension(R.dimen.standard_55));
+        launcherFAB.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+    }
+
+    private void closeFABMenu() {
+        isFABOpen = false;
+        googleFAB.animate().translationX(0);
+        launcherFAB.animate().translationY(0);
+    }
+
+//    public class TestAsync extends AsyncTask<Void, Void, Void> {
+//
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected Void doInBackground(Void... voids) {
+//
+//            // using StringBuffer for immutability
+//            StringBuffer response = null;
+//
+//            // Step 1: create a URL object
+//            URL url = null;
+//
+//            try {
+//                // url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyD0UD9bfUS71Go5KZmbn8esePRit1VPXvg");
+//
+////                url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBMzzVMfEEbuQZAdaSrEDAcZiFpBWWSXB4");
+//                    url = new URL("https://maps.googleapis.com/maps/api/place/search/json?radius=1500&type=restaurant&sensor=false&key=AIzaSyDBxsLNgYurpySX9l34dl6Hw25TySM-bmE&location=-33.8670522,151.1957362");
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            }
+//
+//            try {
+//                // Step 2: HttpURLConnection is an android provided library
+//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                connection.setRequestMethod("GET");
+//
+//                InputStream in = new BufferedInputStream(connection.getInputStream());
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+////                Log.e("Print",reader.toString());
+//                String inputLine;
+//                response = new StringBuffer();
+//                while ((inputLine = reader.readLine()) != null) {
+//                    response.append(inputLine);
+//                    Log.e("Print",inputLine.toString());
+//                }
+//
+//            } catch (ProtocolException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            try {
+//                JSONObject object = new JSONObject(response.toString());
+//
+//                JSONArray resultArray = object.getJSONArray("results");
+//                Log.e("result", resultArray.toString());
+//
+//                if (resultArray.length() == 0) {
+//                    // no results
+//                }
+//                else {
+//                    int length = resultArray.length();
+//                    for (int i=0; i<length; i++) {
+//                        Toast.makeText(getApplicationContext(), resultArray.getJSONObject(i).getString("name") , Toast.LENGTH_LONG).show();
+//                    }
+//
+//                }
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return null;
+//        }
+//    }
+
 
 }
--- app/src/main/java/com/example/googlemapstestproject/ClusterRenderer.java
+++ app/src/main/java/com/example/googlemapstestproject/ClusterRenderer.java
@@ -1,8 +1,13 @@
 package com.example.googlemapstestproject;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,17 +20,29 @@ import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import static com.example.googlemapstestproject.R.drawable.google_icon;
+import java.math.BigDecimal;
 
 public class ClusterRenderer extends ClusterActivity implements ClusterManager.OnClusterItemClickListener<MyItem>,
         ClusterManager.OnClusterInfoWindowClickListener<MyItem>, GoogleMap.OnInfoWindowClickListener {
 
     private ClusterManager<MyItem> clusterManager;
     private GoogleMap googleMap;
-    private JSONArray jsonArray;
+   // private JSONArray jsonArray;
+    private JSONObject jsonObject;
 
     @Override
     public void onInfoWindowClick(Marker marker) {
@@ -38,8 +55,6 @@ public class ClusterRenderer extends ClusterActivity implements ClusterManager.O
 
         }
     }
-
-
 
     @Override
     public boolean onClusterItemClick(MyItem item) {
@@ -57,14 +72,12 @@ public class ClusterRenderer extends ClusterActivity implements ClusterManager.O
     @Override
     protected void startDemo(GoogleMap g) {
 
-
-
 //        Log.e("Print","Sanam guuu");
         googleMap = g;
         clusterManager = new ClusterManager<>(this, googleMap);
 
         // Position the map.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(33.8992734, 35.4811849), 15));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.8688197, 151.2092955), 15));
 
         // Initialize the manager with the context and the map.
         // (Activity extends context, so we can pass 'this' in the constructor.)
@@ -81,52 +94,91 @@ public class ClusterRenderer extends ClusterActivity implements ClusterManager.O
       //  googleMap.setInfoWindowAdapter(clusterManager.getMarkerManager());
 
 
-        jsonArray = readAssets();
+//        jsonArray = readAssets();
+
+        // for new JSON file
+        jsonObject = readAssets();
 
         // Add cluster items (markers) to the cluster manager.
         addItems();
 
 
-
-
        // clusterManager.getMarkerCollection().setOnInfoWindowAdapter(new MyCustomAdapterForItems(inflater));
     }
 
+//    private void addItems() {
+//
+//
+//        // Set some lat/lng coordinates to start with.
+//
+//        try {
+//
+//            for (int i = 0; i < jsonArray.length(); i++) {
+//                JSONObject jsonObject = jsonArray.getJSONObject(i);
+//                double lat = jsonObject.getDouble("lat");
+//                double lng = jsonObject.getDouble("lng");
+//                LatLng latLng = new LatLng(lat, lng);
+////                Log.e("Latitude",Double.toString(lat));
+//              //  MyItem offsetItem = new MyItem(lat, lng);
+//
+//                String title = "Location Name";
+//
+//                String snippet = "Snippet";
+//
+//                MyItem infoWindowItem = new MyItem(latLng, title, snippet);
+//
+//
+//                // Add the cluster item (marker) to the cluster manager.
+//                clusterManager.addItem(infoWindowItem);
+//
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+
+
+    // recreating addItems() for this new JSON file
     private void addItems() {
-
-
-        // Set some lat/lng coordinates to start with.
 
         try {
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                double lat = jsonObject.getDouble("lat");
-                double lng = jsonObject.getDouble("lng");
+            JSONArray resultsArray = jsonObject.getJSONArray("results");
+//            Log.e("REsults Array: ", resultsArray.toString());
+
+            for (int i=0; i<resultsArray.length(); i++) {
+                JSONObject resultsObject = resultsArray.getJSONObject(i);
+                String name = resultsObject.getString("name");
+//                Log.e("Name: ", name);
+
+                String vicinity = resultsObject.getString("vicinity");
+//                Log.e("Vicinity: ", vicinity);
+
+                JSONObject geometryObject = resultsObject.getJSONObject("geometry");
+//                Log.e("Geometry: ", geometryObject.toString());
+
+                JSONObject locationObject = geometryObject.getJSONObject("location");
+//                Log.e("Location: ", locationObject.toString());
+
+                Double lat = Double.parseDouble(locationObject.getString("lat"));
+                Double lng = Double.parseDouble(locationObject.getString("lng"));
+
+//                Log.e("Lat", lat.toString());
                 LatLng latLng = new LatLng(lat, lng);
-//                Log.e("Latitude",Double.toString(lat));
-                MyItem offsetItem = new MyItem(lat, lng);
 
-                String title = "Location Name";
+                MyItem infoWindowItem = new MyItem(latLng, name, vicinity);
 
-                String snippet = "Snippet";
-
-                MyItem infoWindowItem = new MyItem(latLng, title, snippet);
-
-
-                // Add the cluster item (marker) to the cluster manager.
                 clusterManager.addItem(infoWindowItem);
-
             }
-        }catch (Exception e){
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    // reading the data
-    private JSONArray readAssets() {
-//        Log.e("Print","Sanam GUUU");
-        try{
+    // for new JSON file
+    private JSONObject readAssets() {
+        try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("JSONFile")));
             String line = "";
             StringBuilder stringBuilder = new StringBuilder("");
@@ -134,12 +186,62 @@ public class ClusterRenderer extends ClusterActivity implements ClusterManager.O
                 stringBuilder.append(line);
             }
 
-            return new JSONArray(stringBuilder.toString());
+            return new JSONObject(stringBuilder.toString());
 
         }catch (Exception e){
             e.printStackTrace();
         }
-        return new JSONArray();
+        return new JSONObject();
     }
 
+
+    // reading the data from the asset JSON file
+//    private JSONArray readAssets() {
+////        Log.e("Print","Sanam GUUU");
+//        try{
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("JSONFile")));
+//            String line = "";
+//            StringBuilder stringBuilder = new StringBuilder("");
+//            while ((line = bufferedReader.readLine()) != null){
+//                stringBuilder.append(line);
+//            }
+//
+//            return new JSONArray(stringBuilder.toString());
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return new JSONArray();
+//    }
+
+    // option menu: this needs to be at the bottom of the screen
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected item: " + item.getTitle(),Toast.LENGTH_SHORT).show();
+
+        switch (item.getItemId()) {
+
+            case R.id.google_icon:
+                return true;
+
+            case R.id.listview_icon:
+                return true;
+
+//            case item.setIcon(google_icon):
+//                return true;
+//
+//            case item.setIcon(R.drawable.ic_launcher_foreground):
+//                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
 }
